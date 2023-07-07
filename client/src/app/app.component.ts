9d267f9856fd3b4174046bd00a2ae6b53fe17f71
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-07-07 16:47:49 +0530

    Change code to fetch users from api

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Love link';
+  users:any;
+
+  constructor(private http:HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get("https://localhost:5001/api/users").subscribe({
+      next:response=>this.users=response,
+      error:error=>console.log(error),
+      complete:()=>console.log("Request has completed")  
+    })
+  }
 }
