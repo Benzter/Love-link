27277e4029ff03505526127da7a93d0adbd9b767
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-07-16 13:50:51 +0530

    In nav component removed loggedIn property and added new observable property

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../services/account.service';
+import { Observable, of } from 'rxjs';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-nav',
@@ -8,24 +10,25 @@ import { AccountService } from '../services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {}
-  loggedIn = false;
+  currentUser$ : Observable<User | null> = of(null);
 
   constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
+    this.currentUser$ = this.accountService.currentUser$;
   }
+
 
   login(){
     this.accountService.login(this.model).subscribe({
       next:response=>{
         console.log(response);
-        this.loggedIn = true;
       },
       error:err => console.log(err)       
     })
   }
 
   logout(){
-    this.loggedIn = false;
+    this.accountService.logout();
   }
 }
