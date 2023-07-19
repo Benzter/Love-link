bafb0a50409ac54ada4495c6bab27cdd64dc2a9c
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-07-19 12:35:19 +0530

    Remove third party package and add Shared module to imports

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http'
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -14,7 +13,7 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import { ToastrModule } from 'ngx-toastr';
+import { SharedModule } from './_modules/shared.module';
 
 @NgModule({
   declarations: [
@@ -33,11 +32,7 @@ import { ToastrModule } from 'ngx-toastr';
     HttpClientModule,
     FormsModule ,
     BrowserAnimationsModule,
-    BsDropdownModule.forRoot(),
-    BrowserAnimationsModule,
-    ToastrModule.forRoot({
-      positionClass: "toast-bottom-center"
-    })
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
