815cbe9864d9679b973c8bb922fdf965278eb6a9
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-07-21 19:18:56 +0530

    Update UserRepository to include photos when responding to client

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -1,7 +1,6 @@
 using API.Entities;
 using API.Interfaces;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace API.Data
 {
@@ -19,12 +18,16 @@ namespace API.Data
 
         public async Task<AppUser> GetUserByUserNameAsync(string username)
         {
-            return await _context.Users.SingleOrDefaultAsync(x => x.UserName == username);
+            return await _context.Users
+            .Include(p => p.Photos)
+            .SingleOrDefaultAsync(x => x.UserName == username);
         }
 
         public async Task<IEnumerable<AppUser>> GetUsersAsync()
         {
-            return await _context.Users.ToListAsync();
+            return await _context.Users
+            .Include(p => p.Photos)
+            .ToListAsync();
         }
 
         public async Task<bool> SaveAllAsync()
