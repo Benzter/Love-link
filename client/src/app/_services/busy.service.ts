4bc75b2d572ee201059a1ad346f4c63b46732baa
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-08-10 19:19:11 +0530

    Update application to show loading spinner when http request is there

--- client/src/app/_services/busy.service.ts
+++ client/src/app/_services/busy.service.ts
@@ -13,8 +13,8 @@ export class BusyService {
     this.busyRequestCount++;
     this.spinnerService.show(undefined, {
       type: "line-scale-party",
-      bdColor: "rgba(255,255,255,0)",
-      color: "#333333"
+      bdColor: "rgba(255,255,255,0.3)",
+      color: "#fff"
     })
   }
 
