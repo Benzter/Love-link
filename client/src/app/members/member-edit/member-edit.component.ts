7be4c309ed827e7e18ad0e1bf7c6ad7bc740a461
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-08-10 12:56:13 +0530

    Update client program to show unsaved warning messages when user not save edit profiles

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
-import { take } from 'rxjs';
+import { every, take } from 'rxjs';
 import { Member } from 'src/app/_models/member';
 import { User } from 'src/app/_models/user';
 import { AccountService } from 'src/app/_services/account.service';
@@ -14,6 +14,12 @@ import { MembersService } from 'src/app/_services/members.service';
 })
 export class MemberEditComponent implements OnInit {
   @ViewChild("editForm") editForm: NgForm | undefined;
+  @HostListener("window:beforeunload", ["$event"]) unloadNotification($event: any) {
+    if (this.editForm?.dirty){
+      $event.returnValue = true;
+    }
+  }
+
   member: Member | undefined;
   user: User | null = null;
 
